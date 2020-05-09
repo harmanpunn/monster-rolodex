d09d427d9255c6c6337f9b7c20342b6baa3c37d3
832022 <harmanpreet.singh3@ihg.com>
2020-05-10 00:08:21 +0530

    CardList component

--- src/App.js
+++ src/App.js
@@ -1,35 +1,29 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import CardList from "./components/card-list/card-list.component";
 
 class App extends Component {
   constructor() {
     super();
-
     this.state = {
-      monsters: [
-        {
-          name: "Frankenstein",
-          id: "asc1"
-        },
-        {
-          name: "Dracula",
-          id: "asr2"
-        },
-        {
-          name: "Zombie",
-          id: "as1w"
-        }
-      ]
+      monsters: []
     };
+  }
+
+  componentDidMount() {
+    fetch("http://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }));
   }
 
   render() {
     return (
       <div className="App">
-        {this.state.monsters.map(monster => (
-          <h1 key={monster.id}>{monster.name}</h1>
-        ))}
+        <CardList name="harman">
+          {this.state.monsters.map(monster => (
+            <h1 key={monster.id}>{monster.name}</h1>
+          ))}
+        </CardList>
       </div>
     );
   }
